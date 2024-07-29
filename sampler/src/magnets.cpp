11e651ef30f276cf30c368bc97fbc4a4e639f6a5
Sawyer Bergeron <sawyerbergeron@gmail.com>
2024-07-29 10:04:36 -0400

    start doing piecewise linear
    Signed-off-by: Sawyer Bergeron <sawyerbergeron@gmail.com>

--- sampler/src/magnets.cpp
+++ sampler/src/magnets.cpp
@@ -2,11 +2,7 @@
 #include "elapsedMillis.h"
 #include "sampler2.cpp"
 #include "utils.cpp"
-
-struct magnetic_compensator {
-  float sensor_min; // amount to initially offset by
-  float sensor_absolute_max;
-};
+#include <cstddef>
 
 struct longsample_t {
   float value;
@@ -15,6 +11,155 @@ struct longsample_t {
   longsample_t(float value, uint32_t time) : value(value), time(time) {}
   longsample_t() : value(0), time() {}
 };
+
+struct point_t {
+  float x;
+  float y;
+
+  point_t(float x, float y): x(x), y(y) {}
+};
+
+
+
+template <uint32_t POINTS> struct interpolater_t {
+  //point_t points[POINTS];
+  Array<point_t, POINTS> points;
+
+  inline float interpolate(float x) {
+    int idx = 0;
+    for (; idx < POINTS; idx++) {
+      if (this->points[idx].x > x) [[unlikely]] {
+        break;
+      }
+    }
+
+    // point_t p_a;
+    // point_t p_b;
+
+    uint32_t i_a = 0;
+    uint32_t i_b = 0;
+
+    // branchless because it's fun
+    idx -= 1 * (idx == POINTS);
+    idx += 1 * (idx == 0);
+
+    /*if (idx == POINTS) [[unlikely]] {
+      i_a = idx - 2;
+      i_b = idx - 1;
+    } else if (idx == 0) [[unlikely]] {
+      i_a = idx;
+      i_b = idx + 1;
+    } else {
+      // linear between the points
+      i_a = idx - 1;
+      i_b = idx;
+    }*/
+
+    auto p_a = this->points[i_a];
+    auto p_b = this->points[i_b];
+
+    auto top = (p_a.y * (p_b.x - x)) + (p_b.y * (x - p_a.x));
+    auto bottom = p_b.x - p_a.x;
+
+    return top / bottom;
+  }
+
+  template <uint32_t INPUT_POINTS>
+  interpolater_t(Array<point_t, INPUT_POINTS> inputs) {
+      Array<point_t, INPUT_POINTS> sorted;
+
+      // first, sort it by x
+      for(int i = 0; i < inputs.size(); i++) {
+          for(int j = i + 1; i < inputs.size(); j++) {
+              auto p_a = inputs[i];
+              auto p_b = inputs[j];
+
+              if(p_a.x > p_b.x) {
+                  inputs[i] = p_b;
+                  inputs[j] = p_a;
+              }
+          }
+      }
+
+      // now, split into ranges
+      size_t stride = inputs.size() / POINTS;
+
+      for(size_t range_start; range_start < inputs.size(); range_start += stride) {
+          size_t count = 0;
+          float sum_x = 0;
+          float sum_y = 0;
+          for(size_t i = range_start; i < inputs.size() && i < range_start + stride; i++) {
+              count++;
+              auto p = inputs[i];
+              sum_x += p.x;
+              sum_y += p.y;
+          }
+
+          float x = sum_x / count;
+          float y = sum_y / count;
+
+          this->points.push_back({ x, y });
+      }
+  }
+};
+
+/*line_t linear_regression(Array<point_t, 10> &points) {
+  Serial.printf("Have %d datapoints to work with\r\n", points.size());
+  // subtract time of the first point, and value of the minimum
+  uint32_t min_x = points.back().time;
+
+  /float min_val = 1000;
+  for (auto point : points) {
+    if (point.value < min_val) {
+      min_val = point.value;
+    }
+  }*
+
+  float sum_x = 0;
+  float sum_y = 0;
+  float sum_xmy = 0;
+  float sum_xmx = 0;
+  float n = points.size();
+  Serial.println("Profile:");
+  for (auto point : points) {
+    float x = (point.time - min_x);
+    // float y = (point.value - min_val);
+    // float y = this->calibration.normalize_sample(point.value);
+    float y = point.value;
+    // print_normalized(y);
+    Serial.printf("%f @ %f\r\n", y, x);
+    // Serial.flush();
+    sum_x += x;
+    sum_y += y;
+    sum_xmy += x * y;
+    sum_xmx += x * x;
+  }
+
+  float m_x = sum_x / n;
+  float m_y = sum_y / n;
+
+  // Serial.println("Means: " + String(m_x) + ", " + String(m_y));
+
+  float SS_xy = sum_xmy - (n * m_y * m_x);
+  float SS_xx = sum_xmx - (n * m_x * m_x);
+
+  float b_1 = SS_xy / SS_xx;
+   float b_0 = m_y - b_1 * m_x;
+
+  return //;
+}*/
+
+/*template <uint32_t PIECES>
+struct piecewise_t {
+    line_t pieces[PIECES];
+    float min;
+    float max;
+};
+
+struct magnetic_compensator {
+  float sensor_min; // amount to initially offset by
+  float sensor_absolute_max;
+};*/
 
 template <uint32_t BUFSIZE = SAMPLE_BUFFER_LENGTH> struct longsample_buf_t {
   longsample_t buffer[BUFSIZE];
@@ -122,9 +267,9 @@ float linear_regression(Array<longsample_t, 10> &points) {
 }
 
 struct position_t {
-    float distance;
-    float flux;
-    float time;
+  float distance;
+  float flux;
+  float time;
 };
 
 /*bool is_bound_point(longsample_buf_t<4096> &buf, uint32_t pos) {
@@ -247,7 +392,7 @@ static void drop_test(uint8_t bnum, uint8_t pin) {
   uint32_t start_pos;
   uint32_t end_pos;
 
-  // need to find the inflection points that mark the start and end
+  // need to find the points that mark the start and end
   for (int i = midpoint_pos; i < buf.size - 10; i++) {
     if (buf.read_nth_oldest(i).value <= initial_resting + 10) {
       start_pos = i;
@@ -266,9 +411,9 @@ static void drop_test(uint8_t bnum, uint8_t pin) {
   Array<longsample_t, 4096> fall;
 
   // now, we have our bounds
-  for(int i = start_pos; i >= end_pos; i--) {
-      // backwards, because buf is a forgetful queue
-      fall.push_back(buf.read_nth_oldest(i));
+  for (int i = start_pos; i >= end_pos; i--) {
+    // backwards, because buf is a forgetful queue
+    fall.push_back(buf.read_nth_oldest(i));
   }
 
   // now, figure out what the points _should_ be
@@ -284,30 +429,33 @@ static void drop_test(uint8_t bnum, uint8_t pin) {
   // TODO: is this right? do I need to know if these are 0 vs uninit?
   float distance_max = 0;
 
-  for(auto& sample: fall) {
-      uint32_t t_i = sample.time - t_0;
-      float t_if = (float)t_i / 1000000; // convert micros to seconds
+  for (auto &sample : fall) {
+    uint32_t t_i = sample.time - t_0;
+    float t_if = (float)t_i / 1000000; // convert micros to seconds
 
-      float height = 0.5 * g * (t_if * t_if) + (initial_velocity * t_if);
+    float height = 0.5 * g * (t_if * t_if) + (initial_velocity * t_if);
 
-      position_t p;
-      p.distance = -height; // position goes down, but distance goes up as hammer falls
+    position_t p;
+    p.distance =
+        -height; // position goes down, but distance goes up as hammer falls
+    p.flux = sample.value;
 
-
-      // only falling downward, I hope
-      distance_max = min(p.distance, distance_max);
+    // only falling downward, I hope
+    distance_max = min(p.distance, distance_max);
   }
-
 
   // normalize distance to between 0 and 1,
   // 1 being the highest (closest to sensor), 0 being the "bottom"
   // the datapoints currently should have a max near 0,
   // and a min somewhere in the negatives
-  for(auto& p: datapoints) {
-      float cur_p = p.distance;
-      p.distance = cur_p / distance_max;
+  for (auto &p : datapoints) {
+    float cur_p = p.distance;
+    p.distance = cur_p / distance_max;
   }
 
   // now we have a bunch of datapoints mapping flux density
   // to distance, regardless of time
+  //
+  // flux density follows the inverse-cube law
+  //
 }
